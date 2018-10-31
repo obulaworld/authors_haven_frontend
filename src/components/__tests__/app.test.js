b831240ebc458952e4648df60993013dfd9c6f37
koiic <ibraismai1990@gmail>
2018-10-31 02:15:26 +0100

    Fixed merge conflict

--- src/components/__tests__/app.test.js
+++ src/components/__tests__/app.test.js
@@ -1,21 +1,26 @@
 import React from 'react';
+
+// third-party libraries
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+// modules importation
+import Home from '../Home';
+
 configure({ adapter: new Adapter() });
-import App from '../App';
 
 
 //SnapShot Test
 describe('App', () => {
   it('should render correctly in "debug" mode', () => {
-    const component = shallow(<App debug />);
+    const component = shallow(<Home debug />);
     expect(component).toMatchSnapshot();
   });
 
 
   it('should render banner text correctly with given strings', () => {
     const strings = ['one', 'two'];
-    const component = shallow(<App list={strings} />);
+    const component = shallow(<Home list={strings} />);
     expect(component).toMatchSnapshot();
   });
 });
